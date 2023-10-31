001d736b67ae1c87bc978996e31b7aa53dcf1850
thomivargas <thvargas23@gmail.com>
2023-10-31 13:27:23 -0300

    th

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -49,8 +49,8 @@ export default function HeaderPage() {
 
   return (
     <>
-      <header className="w-full h-[100px] bg-[#FFF159]">
-        <div className="w-[62%] pt-2 mx-auto flex items-center justify-between">
+      <header className="w-full h-[100px] bg-[#FFF159] flex flex-col justify-between py-2">
+        <div className="w-[70%] 2xl:w-[62%] flex mx-auto justify-between">
           <Link to='/'>
             <img src={logo} width={180} height={180} alt=""/>
           </Link>
@@ -71,12 +71,12 @@ export default function HeaderPage() {
               </form>
             <img src={disneyHeader} width={335} height={40} alt=""/>
         </div>
-        <div className="w-[62%] pt-2 mx-auto flex items-center justify-between gap-10">
-          <div className="flex items-start gap-2">
+        <div className="xl:w-[70%] 2xl:w-[65%] mx-auto flex items-center justify-around gap-10">
+          <div className="flex items-start xl:gap-2">
             <img src={location} width={18} height={18} alt=""/>
-            <div>
-              <p className="text-xs">Enviar a</p>
-              <p className="text-xs">Capital Federal 1425</p>
+            <div className="text-xs">
+              <p>Enviar a</p>
+              <p>San Juan 5400</p>
             </div>
           </div>
           <nav className="flex list-none gap-5 items-center pt-2 text-xs">
@@ -112,9 +112,9 @@ export default function HeaderPage() {
             <li className="cursor-pointer">AYUDA</li>
           </nav>
           <ul className="flex list-none gap-5 items-center pt-2 text-xs">
+            <li className="cursor-pointer">MIS COMPRAS</li>
             <li className="cursor-pointer">CREA TU CUENTA</li>
             <li className="cursor-pointer">INGRESA</li>
-            <li className="cursor-pointer">MIS COMPRAS</li>
             <Link to='/carrito'>
               <img src={carrito} width={20} alt="" />
             </Link>
